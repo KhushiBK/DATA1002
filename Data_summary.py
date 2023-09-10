5921d4db5bcfc6996f822783d631ca78e9918252
KhushiBK <144584173+KhushiBK@users.noreply.github.com>
2023-09-10 18:37:56 +1000

    Update Data_summary.py

--- Data_summary.py
+++ Data_summary.py
@@ -1,2 +1,19 @@
-import csv
+import pandas as pd
+import numpty as np
+
+
+df=pd.read_csv('Sleep_Efficiency_cleaned.csv')
+
+
+#FINDING Max AND Min of age
+max = df['Age'].max()
+min = df['Age'].min()
+average = df['Age'].mean()
+r_value = df.corr()
+ 
+print(max)
+print(min)
+print(average)
+print(r_value)
+
 
