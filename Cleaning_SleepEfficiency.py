3d2e048382936deb7453eff84610faeb36afa59e
KhushiBK <144584173+KhushiBK@users.noreply.github.com>
2023-09-10 17:23:10 +1000

    Update Cleaning_SleepEfficiency.py

--- Cleaning_SleepEfficiency.py
+++ Cleaning_SleepEfficiency.py
@@ -1,4 +1,3 @@
 import csv
 
-with open("Sleep_Efficiency.csv"):
-  csv_reader = csv.reader(open("Sleep_Efficiency.csv"), "r") as csv file
+csv_reader = csv.reader(open("Sleep_Efficiency.csv"), "r")
